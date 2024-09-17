821dbf57a88fb1ce8bf331807c45f6023b2f9d1a
alexlexlexlex <Alex.rotoni@gmail.com>
2024-09-17 08:30:09 +0800

    Updated

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ACoin"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "ACoin"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "AC"
 )
 
 var ID ids.ID
